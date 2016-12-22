054f278fa5b5941ceaed1072f273c2159d587505
Steven Bateman <batemanvo@gmail.com>
2016-12-21 17:57:01 -0800

    Started creating routes. Created post route, started work on put route.

---
+++ lab-steven/route/student-route.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Router = require('express').Router;
+const parseJSON = require('body-parser').json();
+const debug = require('debug')('student:student-router');
+const Student = require('../model/student.js');
+const studentRouter = module.exports = new Router();
+
+studentRouter.post('/api/student', parseJSON, (request, response, next) => {
+  debug('Student router POST: /api/student');
+
+  request.body.timestamp = new Date();
+  new Student(request.body).save()
+  .then(student => response.json(student))
+  .catch(next);
+});
+
+studentRouter.put('/api/student/:id', parseJSON, (request, response, next) => {
+  debug('Student router PUT: /api/student/:id');
+
+  request.body.timestamp = new Date();
+  Student.
+});
