921d077a43aa1a765e684fddc415c846fe5ff5ac
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-22 10:15:42 -0800

    add require statements for dependencies, add debug to listener

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+const debug = require('debug')('bev:server');
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`SERVER RUNNING ON PORT ${PORT}`);
+  debug(`SERVER RUNNING ON PORT ${PORT}`);
 });
