e2b3b500defec1777ccdc81472e284d4b3433bfd
Steven Bateman <batemanvo@gmail.com>
2016-12-21 18:07:26 -0800

    Updated server to use new router.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -2,8 +2,15 @@
 
 const express = require('express');
 const debug = require('debug')('student:server');
+const cors = require('cors');
+const studentRouter = require('./route/student-route.js');
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(studentRouter);
 
 app.listen(PORT, () => {
   debug(`Server started on port ${PORT}.`);
