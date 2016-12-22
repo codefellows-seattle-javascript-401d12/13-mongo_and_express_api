1dd460badfee8440a0131dea617b83fbffd5e609
kcirekcom <erick.f.mock@gmail.com>
2016-12-22 10:34:20 -0800

    required and using pin routes in server

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
+const pinRouter = require('./route/pin-route.js');
 const debug = require('debug')('pin:server');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = 'mongodb://localhost/pinlist';
+const MONGODB_URI = 'mongodb://localhost/pin-board';
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(pinRouter);
 
 app.listen(PORT, () => {
   debug(`server running: ${PORT}`);
