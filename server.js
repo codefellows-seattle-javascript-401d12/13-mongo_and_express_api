8d4d3fb97e742cfeb90e753a168bef82633a32ea
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-27 16:16:56 -0800

    set up directors routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
+const directorRouter = require('./routes/directors-routes.js');
 const movieRouter = require('./routes/movies-routes.js');
 const errors = require('./lib/error-middleware.js');
 const PORT = process.env.PORT || 3000;
@@ -18,6 +19,7 @@ mongoose.connect(MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(directorRouter);
 app.use(movieRouter);
 app.use(errors);
 
