771fd4bf78e9e463a082625d436700feb17f6813
kcirekcom <erick.f.mock@gmail.com>
2016-12-29 20:36:40 -0800

    GET test written for board router

--- test/board-route-test.js
+++ test/board-route-test.js
@@ -45,7 +45,68 @@ describe('Board Routes', function() {
         });
       });
     });
+
+    describe('with an invalid body', function() {
+      it('should return a 400 error', done => {
+        request.post(`${url}/api/board`)
+        .send({})
+        .end(res => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
   });
 
-  
+  describe('GET: /api/board/:id', function() {
+    describe('with a valid body', function() {
+      before(done => {
+        new Board(exampleBoard).save()
+        .then(board => {
+          this.tempBoard = board;
+          return Board.findByIdAndAddPin(board._id, examplePin);
+        })
+        .then(pin => {
+          this.tempPin = pin;
+          done();
+        })
+        .catch(done);
+      });
+
+      after(done => {
+        if (this.tempBoard) {
+          Board.remove({})
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('should return a board', done => {
+        request.get(`${url}/api/board/${this.tempBoard._id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('test board');
+          expect(res.body.pins.length).to.equal(1);
+          expect(res.body.pin[0].title).to.equal(examplePin.title);
+          expect(res.body.pin[0].skill).to.equal(examplePin.skill);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid request', function() {
+      it('should return a 404 error', done => {
+        request.get(`${url}/api/board/3988794549`)
+        .end(res => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('PUT: /api/board/id')
 });
