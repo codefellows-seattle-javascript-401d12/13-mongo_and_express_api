3aaa11cc9c387cf091b2ded060afc0f46023a93e
kcirekcom <erick.f.mock@gmail.com>
2016-12-29 17:21:44 -0800

    created and exported boardSchema

--- model/pin.js
+++ model/pin.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const pinSchema = Schema({
   title: { type: String, required: true },
   skill: { type: String, required: true },
-  timestamp: { type: Date, required: true }
+  boardID: { type: Schema.Types.ObjectId, required: true }
 });
 
 module.exports = mongoose.model('pin', pinSchema);
