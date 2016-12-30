a5d8cfeca588b3ec6186e3271baad49223266e44
kcirekcom <erick.f.mock@gmail.com>
2016-12-29 18:33:02 -0800

    modified server.js file to use boardRouter

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const Promise = require('bluebird');
-const mongoose = require('mongoose');
 const debug = require('debug')('pin:server');
 
+const boardRouter = require('./route/board-route.js');
 const pinRouter = require('./route/pin-route.js');
+const errors = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/pin';
-
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
@@ -20,7 +21,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 
+app.use(boardRouter);
 app.use(pinRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`server running: ${PORT}`);
