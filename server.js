e1c5f7f4914e877eb130c7d1acf64dfe9ea0fdce
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-02 12:36:43 -0800

    add references to mongoose

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const debug = require('debug')('bev:server');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost/bev';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, () => {
   debug(`SERVER RUNNING ON PORT ${PORT}`);
