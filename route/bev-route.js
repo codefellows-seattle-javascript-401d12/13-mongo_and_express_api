125091a0e59a5959f13305a7ceca16cfb8a100e2
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-03 23:00:38 -0800

    set up BEV router

---
+++ route/bev-route.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Router = require('express').Router;
+const parseJSON = require('body-parser').json();
+const createError = require('http-errors');
+
+const BEV = require('../model/bev.js');
+const bevRouter = module.exports = new Router();
+
+// TODO: Build out routes
+bevRouter.post()
+
+bevRouter.get()
+
+bevRouter.put()
+
+bevRouter.delete()
