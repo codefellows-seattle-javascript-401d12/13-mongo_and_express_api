7ec7e9a6660aec677c690fd0a3fce4528fd79d4e
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-04 00:25:59 -0800

    update app dependencies with new routes, middleware

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const Promise = require('bluebird');
 const mongoose = require('mongoose');
 const debug = require('debug')('bev:server');
 
+const bevRouter = require('./route/bev-route.js');
+const errors = require('./lib/error-middleware.js');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb://localhost/bev';
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bevRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`SERVER RUNNING ON PORT ${PORT}`);
