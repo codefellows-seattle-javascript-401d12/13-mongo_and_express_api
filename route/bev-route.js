b74582dcdaf04f59531322b7539c127e7d91e329
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-04 00:03:43 -0800

    finish building out BEV routes

--- route/bev-route.js
+++ route/bev-route.js
@@ -7,11 +7,30 @@ const createError = require('http-errors');
 const BEV = require('../model/bev.js');
 const bevRouter = module.exports = new Router();
 
-// TODO: Build out routes
-bevRouter.post()
+bevRouter.post('/api/bev', parseJSON, function(req, res, next) {
+  req.body.lastupdated = new Date();
+  new BEV(req.body).save()
+  .then( vehicle => res.json(vehicle))
+  .catch(next);
+});
 
-bevRouter.get()
+bevRouter.get('/api/bev/:id', function(req, res, next) {
+  BEV.findById(req.params.id)
+  .then( vehicle => res.json(vehicle))
+  .catch( err => next(createError(404, err.message)));
+});
 
-bevRouter.put()
+bevRouter.put('/api/bev/:id', parseJSON, function(req, res, next) {
+  BEV.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then( vehicle => res.json(vehicle))
+  .catch( err => {
+    if (err.name === 'ValidationError') return next(err);
+    next(createError(404, err.message));
+  });
+});
 
-bevRouter.delete()
+bevRouter.delete('/api/bev/:id', function(req, res, next) {
+  BEV.findByIdAndRemove(req.params.id)
+  .then( () => res.status(204).send())
+  .catch( err => next(createError(404, err.message)));
+});
