d555c502284f4fbaf7dacda6feab0d95e646c342
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-05 22:21:40 -0800

    write tests for BEV routes with invalid or missing-resource requests

--- test/bev-route-test.js
+++ test/bev-route-test.js
@@ -43,6 +43,37 @@ describe('BEV routes', function() {
         });
       });
     });
+
+    describe('with no content in the request body', function() {
+      it('should return a 400 \'bad request\' status', done => {
+        request.post(`${url}/api/bev`)
+        .send()
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          expect(res.status).to.equal(400);
+          expect(res.res.statusMessage).to.equal('Bad Request');
+          done();
+        })
+      })
+    })
+
+    describe('with required fields missing', function() {
+      var badExampleVehicle = {
+        vehicle: 'bad test vehicle',
+        info: 'bad test info',
+      };
+
+      it('should return a 400 \'bad request\' status', done => {
+        request.post(`${url}/api/bev`)
+        .send(badExampleVehicle)
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          expect(res.status).to.equal(400);
+          expect(res.res.statusMessage).to.equal('Bad Request');
+          done();
+        })
+      })
+    })
   });
 
   describe('GET: /api/bev/:id', function() {
@@ -79,11 +110,52 @@ describe('BEV routes', function() {
         });
       });
     });
+
+    describe('to an invalid id', function() {
+      it('should return a 404 \'not found\' status', done => {
+        request.get(`${url}/api/bev/invalid-id`)
+        .end((err, res) => {
+          expect(err.status).to.equal(404);
+          expect(res.status).to.equal(404);
+          expect(res.res.statusMessage).to.equal('Not Found');
+          done();
+        })
+      })
+    })
   });
 
   describe('GET: /api/bev/', function() {
     describe('with a valid request and data existing in the database', function() {
-      // TODO: build out fetch all ids test
+      before( done => {
+        exampleVehicle.lastupdated = new Date();
+        new BEV(exampleVehicle).save()
+        .then( vehicle => {
+          this.tempVehicle = vehicle;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if (this.tempVehicle) {
+          BEV.remove({})
+          .then( () => done())
+          .catch(done);
+          return;
+        };
+        done();
+      });
+
+      it('should return an array of ids', done => {
+        request.get(`${url}/api/bev`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body).to.include(this.tempVehicle._id.toString());
+          done();
+        })
+      })
     });
   });
 
@@ -130,10 +202,62 @@ describe('BEV routes', function() {
         });
       });
     });
+
+    describe('with no content in the request body', function() {
+      before( done => {
+        exampleVehicle.lastupdated = new Date();
+        new BEV(exampleVehicle).save()
+        .then( vehicle => {
+          this.tempVehicle = vehicle;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if (this.tempVehicle) {
+          BEV.remove({})
+          .then( () => done())
+          .catch(done);
+          return;
+        };
+        done();
+      });
+
+      it('should return a 400 \'bad request\' status', done => {
+        request.put(`${url}/api/bev/${this.tempVehicle._id}`)
+        .send()
+        .end((err, res) => {
+          expect(err.status).to.equal(400);
+          expect(res.status).to.equal(400);
+          expect(res.res.statusMessage).to.equal('Bad Request');
+          done();
+        });
+      });
+    });
+
+    describe('to an invalid id', function() {
+      it('should return a 404 \'not found\' status', done => {
+        var updatedVehicle = {
+          vehicle: 'updated vehicle',
+          info: 'updated info',
+          lastupdated: new Date()
+        };
+
+        request.put(`${url}/api/bev/invalid-id`)
+        .send(updatedVehicle)
+        .end((err, res) => {
+          expect(err.status).to.equal(404);
+          expect(res.status).to.equal(404);
+          expect(res.res.statusMessage).to.equal('Not Found');
+          done();
+        });
+      });
+    });
   });
 
   describe('DELETE: /api/bev/:id', function() {
-    describe('with a valid body', function() {
+    describe('to a valid id', function() {
       before( done => {
         exampleVehicle.lastupdated = new Date();
         new BEV(exampleVehicle).save()
