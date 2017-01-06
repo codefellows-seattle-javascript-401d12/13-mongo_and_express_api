98bc7d9cbee83647135954eaad02fc1b3c11c35b
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-06 10:32:18 -0800

    add integer validator to rating property

--- model/review.js
+++ model/review.js
@@ -7,7 +7,16 @@ const reviewSchema = Schema ({
   title: { type: String, required: true },
   authorName: { type: String, required: true },
   reviewText: { type: String, required: true },
-  rating: { type: Number, min: 1, max: 10, required: true },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+    required: true,
+    validate: {
+      validator: Number.isInteger,
+      message: 'rating must be a whole number between 1 and 10.'
+    }
+  },
   createdOn: { type: Date, default: Date.now },
   reviewID: { type: Schema.Types.ObjectId, required: true }
 });
