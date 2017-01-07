dec5ef7a7c7422a7e98aa5320cf05adcde308895
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-06 18:52:20 -0800

    add review GET route

--- route/review-route.js
+++ route/review-route.js
@@ -3,6 +3,8 @@
 const Router = require('express').Router;
 const parseJSON = require('body-parser').json();
 const BEV = require('../model/bev.js');
+const Review = require('../model/review.js');
+const createError = require('http-errors');
 
 const reviewRouter = module.exports = new Router();
 
@@ -10,4 +12,13 @@ reviewRouter.post('/api/bev/:vehicleID/review', parseJSON, function(req, res, ne
   BEV.findByIdAndAddReview(req.params.vehicleID, req.body)
   .then( review => res.json(review))
   .catch(next);
+});
+
+reviewRouter.get('/api/bev/:vehicleID/review/:id', function(req, res, next) {
+  Review.findById(req.params.id)
+  .then( review => {
+    if (review === null) return next(createError(400, 'Bad Request'));
+    res.json(review);
+  })
+  .catch( err => next(createError(404, err.message)));
 });
