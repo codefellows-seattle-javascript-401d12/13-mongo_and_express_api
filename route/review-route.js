2e3ea08f4fbb38b6dc33f7b9b38fecadb3ab27d3
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-07 09:59:25 -0800

    build out PUT and DELETE routes

--- route/review-route.js
+++ route/review-route.js
@@ -22,3 +22,25 @@ reviewRouter.get('/api/bev/:vehicleID/review/:id', function(req, res, next) {
   })
   .catch( err => next(createError(404, err.message)));
 });
+
+reviewRouter.put('/api/bev/:vehicleID/review/:id', parseJSON, function(req, res, next) {
+  if (Object.getOwnPropertyNames(req.body).length === 0) return next(createError(400, 'Bad Request'));
+  Review.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then( review => {
+    if (review === null) return next(createError(404, err.message));
+    res.json(review);
+  })
+  .catch( err => {
+    if (err.name === 'ValidationError') return next(err);
+    next(createError(404, err.message));
+  });
+});
+
+reviewRouter.delete('/api/bev/:vehicleID/review/:id', function(req, res, next) {
+  Review.findByIdAndRemove(req.params.id)
+  .then( review => {
+    if (review === null) return next(createError(404, err.message));
+    res.status(204).send();
+  })
+  .catch( err => next(createError(404, err.message)));
+});
