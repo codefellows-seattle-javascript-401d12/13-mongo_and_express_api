3a1336eee3ffcc2f6902e1309e899d592eb6a128
Hawa Abdi <abdih17@hotmail.com>
2017-03-02 10:23:48 -0800

    completed tasks

--- lab-hawa/test/spiritAnimal-route-test.js
+++ lab-hawa/test/spiritAnimal-route-test.js
@@ -144,47 +144,47 @@ describe('SpiritAnimal Routes', function() {
         });
       });
     });
-  //
-  //   describe('with an invalid request', function() {
-  //     it('should return a 404 error', done => {
-  //       request.put(`${url}/api/spiritAnimal/3988794549`)
-  //       .end(res => {
-  //         expect(res.status).to.equal(404);
-  //         done();
-  //       });
-  //     });
-  //   });
-  // });
-  //
-  // // describe('DELETE: /api/spiritAnimal/:id', function() {
-  //   describe('with a valid body', function() {
-  //     before(done => {
-  //       new SpiritAnimal(exampleSpiritAnimal).save()
-  //       .then(spiritAnimal => {
-  //         this.tempSpiritAnimal = spiritAnimal;
-  //         done();
-  //       })
-  //       .catch(done);
-  //     });
-  //
-  //     it('should delete a spiritAnimal and return a 204 error', done => {
-  //       request.delete(`${url}/api/spiritAnimal/${this.tempSpiritAnimal._id}`)
-  //       .end((err, res) => {
-  //         if (err) return done(err);
-  //         expect(res.status).to.equal(204);
-  //         done();
-  //       });
-  //     });
-  //   });
-  //
-  //   describe('with an invalid request', function() {
-  //     it('should return a 404 error', done => {
-  //       request.delete(`${url}/api/spiritAnimal/3988794549`)
-  //       .end(res => {
-  //         expect(res.status).to.equal(404);
-  //         done();
-  //       });
-  //     });
-  //   });
+
+    describe('with an invalid request', function() {
+      it('should return a 404 error', done => {
+        request.put(`${url}/api/spiritAnimal/39`)
+        .end(res => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('DELETE: /api/spiritAnimal/:id', function() {
+    describe('with a valid body', function() {
+      before(done => {
+        new SpiritAnimal(exampleSpiritAnimal).save()
+        .then(spiritAnimal => {
+          this.tempSpiritAnimal = spiritAnimal;
+          done();
+        })
+        .catch(done);
+      });
+
+      it('should delete a spiritAnimal and return a 204 error', done => {
+        request.delete(`${url}/api/spiritAnimal/${this.tempSpiritAnimal._id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid request', function() {
+      it('should return a 404 error', done => {
+        request.delete(`${url}/api/spiritAnimal/3988794549`)
+        .end(res => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
   });
 });
